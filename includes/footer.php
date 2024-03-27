efdcb26d4aaf96d78ddd7702edcb9fb4f36d1dc7
root <root@vps-1abe1e7a.vps.ovh.net>
2024-03-27 14:18:41 +0000

    maj footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="fixed-bottom d-flex justify-content-center align-items-end" ;>
     <div class="container text-center">
-        &copy; <?= date('Y') ?> Jerome Rigoni And Francisco Sepulveda vasquez de marquez
+        &copy; <?= date('Y') ?> Jérôme Rigoni And Francisco Sepulveda vasquez de marquez
     </div>
 </footer>
